82a6cbf9981c7e0e065ad9a522f4bad1d2ac3764
Robert Kania <cezaryrk@yahoo.de>
2018-05-02 12:23:26 +0200

    correct theme integration

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,15 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './../modules/app/app.module';
-import { environment } from './environments/environment';
+// Theme initialization
+import {ThemeRegistry} from 'typexs-ng/libs/angular/ThemeRegistry';
+
+import {THEMES} from './themes';
+import {STYLES} from './stylesheets';
+ThemeRegistry.register(THEMES, STYLES);
+
+import {AppModule} from './../modules/app/app.module';
+import {environment} from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
